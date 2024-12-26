be19a286312d67a005cdd406f34e265ffc4db1b4
PayalPatil8980 <sq.payal30@gmail.com>
2024-12-26 07:37:19 +0530

    linear

--- linear.c
+++ linear.c
@@ -1,46 +1,54 @@
-#include<stdio.h>
-int serach(int a[],int n,int val){
-    for(int i=0;i<n;i++){
-        if(a[i]==val){
-            return i+1;
+#include <stdio.h>
+int serach(int a[], int n, int val)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == val)
+        {
+            return i + 1;
         }
     }
     return -1;
 }
 
-int main(){
-    int a[5],i;
+int main()
+{
+    int a[5], i;
     int val;
-    int n=sizeof(a) / sizeof(a[0]);
-    for(i=0;i<n;i++){
+    int n = sizeof(a) / sizeof(a[0]);
+    for (i = 0; i < n; i++)
+    {
         printf("Enter the elements: ");
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
-    for(i=0;i<n;i++){
-        printf("%d\t",a[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
     }
     printf("\n");
     printf("Enter the elemnt to search: ");
-    scanf("%d",&val);
-    int res=serach(a,n,val);
-    printf("The element of array are- %d ",val);
+    scanf("%d", &val);
+    int res = serach(a, n, val);
+    printf("The element of array are- %d ", val);
     printf("\n");
-    if(res==-1){
+    if (res == -1)
+    {
         printf("The element is not in array");
     }
-    else{
-        printf("The element is in position of: %d %d",res,res);
+    else
+    {
+        printf("The element is in position of: %d", res);
     }
 }
 /*#include <stdio.h>
 
 void search(int a[], int n, int val) {
-    int positions[n]; 
-    int count = 0;    
+    int positions[n];
+    int count = 0;
 
     for (int i = 0; i < n; i++) {
         if (a[i] == val) {
-            positions[count++] = i + 1; 
+            positions[count++] = i + 1;
         }
     }
 
@@ -74,7 +82,7 @@ int main() {
     printf("Enter the element to search: ");
     scanf("%d", &val);
 
-    search(a, n, val); 
+    search(a, n, val);
 
     return 0;
 }
